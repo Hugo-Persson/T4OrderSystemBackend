cc99b9c43634119a6c18b1a805f7adcdedf4ec51
Hugo Persson <hugper0119@skynet.halmstad.se>
2019-12-03 08:42:23 +0100

    capital S

--- StaticRouter.js
+++ StaticRouter.js
@@ -1,5 +1,5 @@
 module.exports = (app, express) => {
-    app.use(express.static(process.cwd() + "/static"));
+    app.use(express.static(process.cwd() + "/Static"));
     app.get("/", (req, res) => {
         res.sendFile(process.cwd() + "/index.html")
     });
