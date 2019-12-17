fbc76dbe716e2e0f1420282e7d670a1b6d4713df
Hugo Persson <hugper0119@skynet.halmstad.se>
2019-12-17 09:22:08 +0100

    Commented

--- Modules/Email.js
+++ Modules/Email.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 module.exports = {
     sendVerificationCode: sendVerificationCode
 }
+// Here you set the email credentials 
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -13,15 +14,17 @@ const transporter = nodemailer.createTransport({
 
 function sendVerificationCode(to, code) {
     const mailOptions = {
-        from: "noreply@something.com",
+        from: "noreply@gmail.com", // this is what the client should see that it is from
         to: to,
         subject: "Verification code",
         html: codeEmailBody(code)
     }
+    // This returns the promise
     return transporter.sendMail(mailOptions);
 }
 
 function codeEmailBody(code) {
+    // This just creates the body for the email that the user will see when they get the code
     return `
     <div>
     <h2><b>Your verification code is <span style="text-decoration:underline">${code}</span></b></h2>
